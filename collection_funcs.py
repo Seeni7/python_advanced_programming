c91013c6b061e7def8592f23ef2240a065b96adf
seeni7 <Seeni.rengasamy@gmail.com>
2020-12-28 14:49:19 -0500

    higher order function

---
+++ collection_funcs.py
@@ -0,0 +1,13 @@
+from functools import reduce
+
+domain = [1, 2, 3, 4, 5]
+
+# f(x) x * 2
+our_range = map(lambda num: num * 2, domain)
+print(list(our_range))
+
+evens = filter(lambda num: num % 2 == 0, domain)
+print(list(evens))
+
+the_sum = reduce(lambda acc, num: acc + num, domain, 0)
+print(the_sum)
