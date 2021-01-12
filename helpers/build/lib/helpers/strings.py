15a64604730de10e828a754fdb09a050fc2068bb
seeni7 <Seeni.rengasamy@gmail.com>
2021-01-12 10:16:23 -0500

    DocString

---
+++ helpers/build/lib/helpers/strings.py
@@ -0,0 +1,14 @@
+#__all__ = ['extract_upper']
+
+
+def extract_upper(phrase):
+    return list(filter(str.isupper, phrase))
+
+def extract_lower(phrase):
+    return list(filter(str.islower, phrase))
+
+
+#if __name__ == "__main__":
+#    print("HELLO FROM HELPERS")
+
+
---
+++ helpers/build/lib/helpers/variables.py
@@ -0,0 +1,7 @@
+import helpers
+
+
+
+name = "Seeni Rengasamy"
+
+
